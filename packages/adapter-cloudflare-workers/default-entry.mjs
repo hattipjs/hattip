023ba77f5ae3086bd9e2e34e22293b8e5c79854c
cyco130 <cyco130@gmail.com>
2022-05-20 13:04:44 +0300

    feat: remove default entries

--- packages/adapter-cloudflare-workers/default-entry.mjs
+++
@@ -1,6 +0,0 @@
-import cloudflareWorkersAdapter from "./dist/index.mjs";
-import handler from "virtual:hattip:handler-entry";
-
-export default {
-  fetch: cloudflareWorkersAdapter(handler),
-};
--- packages/vite-cloudflare-workers/src/default-entry.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "@hattip/adapter-cloudflare-workers/default-entry";
