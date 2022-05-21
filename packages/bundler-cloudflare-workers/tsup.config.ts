dae0cf5e0742556bc414b0cb5fd84eef6844caa9
cyco130 <cyco130@gmail.com>
2022-05-21 23:08:05 +0300

    fix: clean up packages and fix build errors

--- packages/bundler-cloudflare-workers/tsup.config.ts
+++ packages/bundler-cloudflare-workers/tsup.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from "tsup";
 export default defineConfig([
   {
     entry: ["./src/index.ts", "./src/cli.ts"],
-    format: ["esm", "cjs"],
+    format: ["esm"],
+    platform: "node",
+    target: "node14",
+  },
+  {
+    entry: ["./src/index.ts"],
+    format: ["cjs"],
     platform: "node",
     target: "node14",
     dts: true,
