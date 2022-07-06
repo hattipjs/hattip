637627eb98a5c72e895a964bd80c6ac8ef710a8e
cyco130 <cyco130@gmail.com>
2022-07-06 12:49:38 +0300

    chore: fix linting errors

--- packages/adapter/adapter-node/src/index.ts
+++ packages/adapter/adapter-node/src/index.ts
@@ -1,17 +1,7 @@
 // TODO: Remove or update this rule!
 /* eslint-disable import/no-unresolved */
-import type { HattipHandler } from "@hattip/core";
-import {
-  createServer as createHttpServer,
-  Server as HttpServer,
-  ServerResponse,
-  ServerOptions,
-} from "http";
-import {
-  createMiddleware as createNodeMiddleware,
-  DecoratedRequest,
-  NodeAdapterOptions,
-} from "./common";
+import { ServerResponse } from "http";
+import { DecoratedRequest, NodeAdapterOptions } from "./common";
 import installNodeFetch from "@hattip/polyfills/node-fetch";
 import installGetSetCookie from "@hattip/polyfills/get-set-cookie";
 
