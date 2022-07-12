0beaa3d082d31ebf275d694c59ad13bd196e3267
cyco130 <cyco130@gmail.com>
2022-07-12 12:05:51 +0300

    feat(bun): add static file handling

--- packages/adapter/adapter-bun/readme.md
+++ packages/adapter/adapter-bun/readme.md
@@ -4,11 +4,35 @@ HatTip adapter for [Bun](https://bun.sh).
 
 ## Usage
 
-Assuming you have your HatTip handler defined in `handler.js`, create an `entry-bun.js` file like the following and use `bun entry-bun.js` to run it:
+Assuming you have your HatTip handler defined in `handler.js` and your static assets are in the `public` directory, create an `entry-bun.js` file like the following and use `bun entry-bun.js` to run it:
 
 ```ts
 import bunAdapter from "@hattip/adapter-bun";
 import handler from "./handler.js";
+import url from "url";
+import path from "path";
 
-export default bunAdapter(handler);
+const dir = path.resolve(
+  path.dirname(url.fileURLToPath(new URL(import.meta.url))),
+  "public",
+);
+
+export default bunAdapter(handler, { staticDir: dir });
 ```
+
+You can leave out the `staticDir` option if you don't want to serve static assets.
+
+## Options
+
+- `staticDir`: The directory to serve static assets from. Leave undefined to not serve static assets.
+- `trustProxy`: Whether to trust the `X-Forwarded-For` header.
+
+The remaining options (`port`, `hostname` etc.) are passed to [Bun.serve](https://github.com/oven-sh/bun#bunserve---fast-http-server).
+
+## Limitations
+
+Bun support is preliminary and Bun itself is in early development:
+
+- No way to determine the IP address of the client when `trustProxy` is false.
+- No streaming, everything is cached.
+- No way to set multiple `Set-Cookie` headers.
--- packages/adapter/adapter-bun/src/index.ts
+++ packages/adapter/adapter-bun/src/index.ts
@@ -1,12 +1,55 @@
+/// <reference types="bun-types" />
+import fs from "fs";
+import path from "path";
 import type { AdapterRequestContext, HattipHandler } from "@hattip/core";
+import type { Serve as BunServeOptions } from "bun";
 
-export default function bunAdapter(handler: HattipHandler) {
+export type { BunServeOptions };
+
+export type BunAdapterOptions = Omit<BunServeOptions, "fetch" | "error"> & {
+  staticDir?: string;
+  trustProxy?: boolean;
+};
+
+export default function bunAdapter(
+  handler: HattipHandler,
+  options: BunAdapterOptions = {},
+): BunServeOptions {
+  const { staticDir, trustProxy, ...remaingOptions } = options;
+
+  let staticFiles: Set<string>;
+  if (staticDir) {
+    staticFiles = walk(staticDir);
+  }
+
   return {
+    ...remaingOptions,
+
     fetch(request: Request) {
+      if (staticFiles) {
+        let path = new URL(request.url).pathname;
+        if (path.endsWith("/")) {
+          path = path.slice(0, -1);
+        }
+        const fullPath = staticDir + path;
+
+        if (staticFiles.has(path)) {
+          return new Response(Bun.file(fullPath));
+        } else if (staticFiles.has(path + "/index.html")) {
+          return new Response(Bun.file(fullPath + "/index.html"));
+        } else if (staticFiles.has(path + ".html")) {
+          return new Response(Bun.file(fullPath + ".html"));
+        }
+      }
+
       const context: AdapterRequestContext = {
         request,
-        // TODO: How to get the IP address?
-        ip: "127.0.0.1",
+        // TODO: How to get the IP address when not behind a proxy?
+        ip: trustProxy
+          ? String(request.headers.get("x-forwarded-for") || "")
+              .split(",", 1)[0]
+              .trim()
+          : "127.0.0.1",
         passThrough() {
           // No op
         },
@@ -18,5 +61,30 @@ export default function bunAdapter(handler: HattipHandler) {
 
       return handler(context);
     },
+
+    error(error) {
+      console.error(error);
+      return new Response("Internal Server Error", { status: 500 });
+    },
   };
+}
+
+function walk(
+  dir: string,
+  root = dir,
+  entries = new Set<string>(),
+): Set<string> {
+  const files = fs.readdirSync(dir);
+
+  for (const file of files) {
+    const filepath = path.join(dir, file);
+    const stat = fs.statSync(filepath);
+    if (stat.isDirectory()) {
+      walk(filepath, root, entries);
+    } else {
+      entries.add("/" + path.relative(root, filepath).replace(/\\/g, "/"));
+    }
+  }
+
+  return entries;
 }
