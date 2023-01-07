c3f21e6c4838605d522f1f23f64c6c6f9a49baec
QuiiBz <tom.lienrd@gmail.com>
2023-01-07 16:56:48 +0100

    fix: remove setTimeout polyfill

--- testbed/basic/entry-lagon.js
+++ testbed/basic/entry-lagon.js
@@ -2,6 +2,4 @@
 import lagonAdapter from "@hattip/adapter-lagon";
 import hattipHandler from ".";
 
-globalThis.setTimeout = (callback) => Promise.resolve().then(callback);
-
 export const handler = lagonAdapter(hattipHandler);
