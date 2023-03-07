6bec1290bb660c82f0fae8bbfff81569ff710cdd
cyco130 <cyco130@gmail.com>
2023-03-07 10:59:17 +0300

    release: 0.0.30

--- packages/base/sse/lint-staged.config.mjs
+++
@@ -1,7 +0,0 @@
-export default {
-	"**/*.ts?(x)": [
-		() => "tsc -p tsconfig.json --noEmit",
-		"eslint --max-warnings 0 --ignore-pattern dist",
-	],
-	"*": "prettier --ignore-unknown --write",
-};
